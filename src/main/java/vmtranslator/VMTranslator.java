a9371c1994882a6e111bd10bcd7baef391f92abf
matheusryann <matheus.ryan@discente.ufma.br>
2026-06-07 18:07:35 -0300

    feat: wire parsers and codewriter to generate

--- src/main/java/vmtranslator/VMTranslator.java
+++ src/main/java/vmtranslator/VMTranslator.java
@@ -1,5 +1,8 @@
 package vmtranslator;
 
+import vmtranslator.codewriter.CodeWriter;
+import vmtranslator.parser.Command;
+import vmtranslator.parser.CommandType;
 import vmtranslator.parser.Parser;
 
 import java.io.IOException;
@@ -11,13 +14,31 @@ public final class VMTranslator {
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
-            System.err.println("Uso inicial: java -jar vmtranslator.jar <arquivo.vm>");
+            System.err.println("Uso: java -jar vmtranslator.jar <arquivo.vm>");
             System.exit(1);
         }
 
-        Parser parser = new Parser(Path.of(args[0]));
-        while (parser.hasMoreCommands()) {
-            System.out.println(parser.advance());
+        Path input = Path.of(args[0]);
+        Path output = asmPathFor(input);
+        Parser parser = new Parser(input);
+
+        try (CodeWriter writer = new CodeWriter(output)) {
+            while (parser.hasMoreCommands()) {
+                Command command = parser.advance();
+                switch (command.type()) {
+                    case C_ARITHMETIC -> writer.writeArithmetic(command.arg1());
+                    case C_PUSH -> writer.writePush(command.arg1(), command.arg2());
+                    case C_POP -> writer.writePop(command.arg1(), command.arg2());
+                }
+            }
         }
+    }
+
+    private static Path asmPathFor(Path input) {
+        String fileName = input.getFileName().toString();
+        if (!fileName.endsWith(".vm")) {
+            throw new IllegalArgumentException("Arquivo de entrada deve ter extensao .vm: " + input);
+        }
+        return input.resolveSibling(fileName.substring(0, fileName.length() - 3) + ".asm");
     }
 }
