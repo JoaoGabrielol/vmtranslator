14d440a130dc3694e98b4bb85342c47f7cd42632
Matheus <matheus.ryan@discente.ufma.br>
2026-06-07 16:38:46 -0300

    feat(codewriter): add file output infrastructure and close method

--- src/main/java/vmtranslator/codewriter/CodeWriter.java
+++ src/main/java/vmtranslator/codewriter/CodeWriter.java
@@ -1,12 +1,17 @@
 package vmtranslator.codewriter;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-public final class CodeWriter {
+public final class CodeWriter implements AutoCloseable {
     private final Path output;
+    private final BufferedWriter writer;
 
-    public CodeWriter(Path output) {
+    public CodeWriter(Path output) throws IOException {
         this.output = output;
+        this.writer = Files.newBufferedWriter(output);
     }
 
     public Path output() {
@@ -23,5 +28,22 @@ public final class CodeWriter {
 
     public void writePop(String segment, int index) {
         throw new UnsupportedOperationException("TODO: implementar comando pop");
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    protected void writeComment(String command) throws IOException {
+        writer.write("// " + command);
+        writer.newLine();
+    }
+
+    protected void write(String... lines) throws IOException {
+        for (String line : lines) {
+            writer.write(line);
+            writer.newLine();
+        }
     }
 }
